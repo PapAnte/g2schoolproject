4abf2c1641cb46858e4597718c656f203c172982
Egzon K <Egzon_Karaliti@hotmail.de>
2017-05-15 11:34:07 +0200

    - Web.config angepasst - DB - Script überarbeitet

--- Bravo/Bravo/SQL_Scripts/SQL_Create.sql
+++ Bravo/Bravo/SQL_Scripts/SQL_Create.sql
@@ -27,62 +27,117 @@ GO
 CREATE TABLE Raum
 (
 	ID int IDENTITY(1,1) CONSTRAINT RaumID PRIMARY KEY,
-	Beschreibung varchar(45),
+	Bezeichnung varchar(45),
 	Notiz varchar(max)	
 );
 GO
 
-CREATE TABLE Komponentenattribute
+CREATE TABLE Software
 (
-	ID int IDENTITY(1,1) CONSTRAINT KomponentenattributeID PRIMARY KEY,
-	Beschreibung varchar(max)
-);
-GO
-
-CREATE TABLE Komponentenart
-(
-	ID int IDENTITY(1,1) CONSTRAINT KomponentenartID PRIMARY KEY,
-	Beschreibung varchar(max)
-);
-GO
-
-CREATE TABLE Komponente
-(
-	ID int IDENTITY(1,1) CONSTRAINT KomponenteID PRIMARY KEY,
-	Beschreibung varchar(max),
-	Hersteller_ID int CONSTRAINT FK_KomponenteHersteller FOREIGN KEY REFERENCES Hersteller(ID),
-	Gewaehrleistung_Monate INT,
-	Art_ID int CONSTRAINT FK_KomponenteKomponentenart FOREIGN KEY REFERENCES Komponentenart(ID),
-	Lieferant_ID int CONSTRAINT FK_Komponentelieferant FOREIGN KEY REFERENCES Lieferant(ID),
+	ID int IDENTITY(1,1) CONSTRAINT SoftwareID PRIMARY KEY,
+	Bezeichnung varchar(max),
+	[Version] varchar(45),
+	Lizenz_Benutzerumfang int,
+	Lizenz_Schlüssel varchar(45),
+	Hersteller_ID int,
+	Lieferanten_ID int,
 	Einkaufsdatum date,
-	Kaufbeleg Binary,
-	Raum_ID int CONSTRAINT FK_KomponenteRaum FOREIGN KEY REFERENCES Raum(ID),
+	Einkaufsbeleg binary,
 	Notiz varchar(max)
 );
 GO
 
-CREATE TABLE Komponenten_Attribut
+CREATE TABLE Software_in_Raum
 (
-	Komponente_ID int CONSTRAINT FK_KomponentenAttribut_Komponente FOREIGN KEY REFERENCES Komponente(ID),
-	Attribute_ID int CONSTRAINT FK_KomponentenAttribut_Komponentenattribute FOREIGN KEY REFERENCES Komponentenattribute(ID),
-	Hersteller_ID int CONSTRAINT FK_KomponentenAttribut_Hersteller FOREIGN KEY REFERENCES Hersteller(ID),
-	Beschreibung varchar(max) 
+	Software_ID int CONSTRAINT FK_SoftwareraumSoftware FOREIGN KEY REFERENCES Software(ID),
+	Raum_ID int CONSTRAINT FK_SoftwareraumRaum FOREIGN KEY REFERENCES Raum(ID)
 );
 GO
 
-CREATE TABLE Komponentenart_Attribute_Vorgabe
+CREATE TABLE Geraetattribut
 (
-	Art_ID int CONSTRAINT FK_KomponentenAttributVorgabe_Komponentenart FOREIGN KEY REFERENCES Komponentenart(ID),
-	Attribute_ID int CONSTRAINT FK_KomponentenAttribut_Komponentenattribute2 FOREIGN KEY REFERENCES Komponentenattribute(ID)
+	ID int IDENTITY(1,1) CONSTRAINT GeraetattributeID PRIMARY KEY,
+	Bezeichnung varchar(max)
+);
+GO
+
+CREATE TABLE Geraetart
+(
+	ID int IDENTITY(1,1) CONSTRAINT GeraetartID PRIMARY KEY,
+	Bezeichnung varchar(max)
+);
+GO
+
+CREATE TABLE Geraet
+(
+	ID int IDENTITY(1,1) CONSTRAINT KomponenteID PRIMARY KEY,
+	Bezeichnung varchar(max),
+	Hersteller_ID int CONSTRAINT FK_GeraetHersteller FOREIGN KEY REFERENCES Hersteller(ID),
+	Gewaehrleistung_Monate INT,
+	Art_ID int CONSTRAINT FK_GeraetGeraetart FOREIGN KEY REFERENCES Geraetart(ID),
+	Lieferant_ID int CONSTRAINT FK_Geraetlieferant FOREIGN KEY REFERENCES Lieferant(ID),
+	Einkaufsdatum date,
+	Einkaufbeleg Binary,
+	Raum_ID int CONSTRAINT FK_GeraetRaum FOREIGN KEY REFERENCES Raum(ID),
+	Notiz varchar(max)
+);
+GO
+
+CREATE TABLE Geraet_Attributinformation
+(
+	Komponente_ID int CONSTRAINT FK_GeraetattributinformationGeraet FOREIGN KEY REFERENCES Geraet(ID),
+	Attribute_ID int CONSTRAINT FK_GeraetattributinformationGeraetattribute FOREIGN KEY REFERENCES Geraetattribut(ID),
+	Hersteller_ID int CONSTRAINT FK_GeraetattributinformationHersteller FOREIGN KEY REFERENCES Hersteller(ID),
+	Bezeichnung varchar(max) 
+);
+GO
+
+CREATE TABLE Geraetart_Attribute_Vorgabe
+(
+	Art_ID int CONSTRAINT FK_GeraetnattributvorgabeGeraetart FOREIGN KEY REFERENCES Geraetart(ID),
+	Attribute_ID int CONSTRAINT FK_GeraetattributGeraetattribute FOREIGN KEY REFERENCES Geraetattribut(ID)
 );
 GO
 
 CREATE TABLE Rechtevorlage
 (
 	ID int IDENTITY(1,1) CONSTRAINT RechevorlageID PRIMARY KEY,
-	Beschreibung varchar(max),
+	Bezeichnung varchar(max),
 	Lesen bit,
 	Bearbeiten bit,
 	Erstellen bit,
 	Entfernen bit
+);
+
+CREATE TABLE Rechtegruppe
+(
+	ID int IDENTITY(1,1) CONSTRAINT RechtegruppeID PRIMARY KEY,
+	Bezeichnung varchar(max),
+	Benutzerverwaltung_Vollzugriff bit,
+	Ticket int,
+	Komponenten int
+);
+
+CREATE TABLE WebBenutzer
+(
+	Nutzername varchar(80) CONSTRAINT WebBenutzerID PRIMARY KEY,
+	Passwort varchar(80),
+	Rechtegruppe_ID int CONSTRAINT FK_WebbenutzerRechtegruppe FOREIGN KEY REFERENCES Rechtegruppe(ID),
+	Name varchar(80),
+	Anrede varchar(15),
+	Titel varchar(15),
+	Geburtstag date,
+	EMail varchar(160),
+	Erstellungsdatum date
+);
+
+CREATE TABLE Ticket
+(
+	ID int IDENTITY(1,1) CONSTRAINT TicketID PRIMARY KEY,
+	Bezeichnung varchar(max),
+	Notiz varchar(max),
+	Ersteller varchar(80) CONSTRAINT FK_TicketWebbenutzerErsteller FOREIGN KEY REFERENCES WebBenutzer(Nutzername),
+	DatumEingang date,
+	Bearbeiter varchar(80) CONSTRAINT FK_TicketWebbenutzerBearbeiter FOREIGN KEY REFERENCES WebBenutzer(Nutzername),
+	AbgeschlossenDatum date	
 );
