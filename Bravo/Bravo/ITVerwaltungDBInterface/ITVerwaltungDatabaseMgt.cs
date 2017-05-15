12956469e3942e632ae358df1ebeb85277a76313
Sebastian Meyer <Sebastian.Meyer.115@outlook.com>
2017-05-15 14:47:18 +0200

    Implemented Basic Interface for Database ITVerwaltung with TemplateFunction

---
+++ Bravo/Bravo/ITVerwaltungDBInterface/ITVerwaltungDatabaseMgt.cs
@@ -0,0 +1,16 @@
+﻿namespace Bravo.ITVerwaltungDBInterface
+{
+	public static class ITVerwaltungDatabaseMgt
+	{
+		public const string DB_SERVER = "";
+		public const string DB_NAME = "";
+		public const string DB_LOGIN_NAME = "";
+		public const string DB_LOGIN_PW = "";
+
+		public static DBResult FunctionTemplate(string infoOne, int infoTwo, out string resultMessage)
+		{
+			resultMessage = "Not implemented!";
+			return DBResult.Error;
+		}
+	}
+}
