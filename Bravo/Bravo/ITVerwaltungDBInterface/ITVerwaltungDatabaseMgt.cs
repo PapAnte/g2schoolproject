386cb0601adb4639cebc2e385e3cc914a5b2fd90
Sebastian Meyer <Sebastian.Meyer.115@outlook.com>
2017-05-15 18:56:47 +0200

    first function template

--- Bravo/Bravo/ITVerwaltungDBInterface/ITVerwaltungDatabaseMgt.cs
+++ Bravo/Bravo/ITVerwaltungDBInterface/ITVerwaltungDatabaseMgt.cs
@@ -1,16 +1,56 @@
-﻿namespace Bravo.ITVerwaltungDBInterface
+﻿using System;
+using System.Data.SqlClient;
+
+namespace Bravo.ITVerwaltungDBInterface
 {
 	public static class ITVerwaltungDatabaseMgt
 	{
 		public const string DB_SERVER = "";
-		public const string DB_NAME = "";
+		public const string DB_NAME = "ITVerwaltung";
 		public const string DB_LOGIN_NAME = "";
 		public const string DB_LOGIN_PW = "";
+		public const int DB_CONN_TIMEOUT = 15;
+
+		public static DBResult TryLogin(string username, string password, out string resultMessage)
+		{
+			// TODO
+			resultMessage = "Funktion nicht implementiert!";
+			return DBResult.Error;
+		}
 
 		public static DBResult FunctionTemplate(string infoOne, int infoTwo, out string resultMessage)
 		{
-			resultMessage = "Not implemented!";
+			// TODO
+			resultMessage = "Funktion nicht implementiert!";
 			return DBResult.Error;
+		}
+
+		/// <summary>
+		/// Versucht, eine Verbindung zur Datenbank 'ITVerwaltung' aufzubauen.
+		/// Wenn Vorgang abgeschlossen, muss die Verbindung wieder geschlossen werden! -> sqlConnection.Close()
+		/// </summary>
+		/// <param name="connection">Exception-Fehlermeldung, falls nicht erfolgreich.</param>
+		/// <returns>SqlConnection-Objekt - null, wenn nicht erfolgreich.</returns>
+		private static SqlConnection TryOpenSqlConnection(out string exceptionString)
+		{
+			exceptionString = "";
+			try
+			{
+				SqlConnection connection = new SqlConnection(
+					$"user id={DB_LOGIN_NAME};"
+					+ $"Password={DB_LOGIN_PW};"
+					+ $"Server={DB_SERVER};"
+					+ $"Trusted_Connection=yes;"
+					+ $"Database={DB_NAME};"
+					+ $"Connection Timeout={DB_CONN_TIMEOUT}");
+				connection.Open();
+				return connection;
+			}
+			catch (Exception e)
+			{
+				exceptionString = e.ToString();
+				return null;
+			}
 		}
 	}
 }
